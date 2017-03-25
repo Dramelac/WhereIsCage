d82d62a9833c2223a54c2f8da6b1f7dbab665915
Dramelac <203697@supinfo.com>
2017-03-25 21:05:51 +0100

    Update navs menu + title + parents

--- WhereIsCage/app/src/main/java/com/supinfo/app/whereiscage/ScoreboardActivity.java
+++ WhereIsCage/app/src/main/java/com/supinfo/app/whereiscage/ScoreboardActivity.java
@@ -54,29 +54,6 @@ public class ScoreboardActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scoreboard, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -131,7 +108,6 @@ public class ScoreboardActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return 3;
         }
 
